ba4cb9501d7666a2aec097c08c10d7c53b0836e8
Pranay Singh <ipranaysingh@gmail.com>
2024-06-19 19:12:59 +0530

    add pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+
+
 
 
 function App() {
   return (
     <>
-      <h1 className='text-2xl'>Namaste</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile" element={<Profile />}/>
+          <Route path="/sign-in" element={<SignIn />}/>
+          <Route path="/sign-up" element={<SignUp />}/>
+          <Route path="/offers" element={<Offers />}/>
+          <Route path="/forgot-password" element={<ForgotPassword />}/>
+        </Routes>
+      </Router>
     </>
   );
 }
